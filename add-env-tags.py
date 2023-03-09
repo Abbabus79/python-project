3df139055fe3127368658796a44ddd5a46a23d6a
“papis1984” <nkengfac@ualberta.ca>
2023-03-09 14:47:00 -0700

    fixed AZ zone

--- add-env-tags.py
+++ add-env-tags.py
@@ -9,14 +9,14 @@ ec2_resource_frankfurt = boto3.resource('ec2', region_name="eu-central-1")
 instance_ids_paris = []
 instance_ids_frankfurt = []
 
-reservations_paris = ec2_client_paris.describe_instances()['Reservations']
+reservations_paris = ec2_client_oregon.describe_instances()['Reservations']
 for res in reservations_paris:
     instances = res['Instances']
     for ins in instances:
         instance_ids_paris.append(ins['InstanceId'])
 
 
-response = ec2_resource_paris.create_tags(
+response = ec2_resource_oregon.create_tags(
     Resources=instance_ids_paris,
     Tags=[
         {
